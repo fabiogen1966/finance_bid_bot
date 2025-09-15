4af46937fab979724b722573a2ed6887ef7767f2
gensoftdev@gmail.com <gensoftdev@gmail.com>
2025-09-15 20:45:05 +0200

    bot

--- config.py
+++ config.py
@@ -15,5 +15,20 @@ SYMBOL_UNITS = {
     'rame': 'USD/LB'
 }
 
+# Telegram Bot Configuration
+TELEGRAM_BOT_TOKEN = '<YOUR_BOT_TOKEN>'
+TELEGRAM_CHAT_ID = '<YOUR_CHAT_ID>'
+
+# Polling interval in seconds
+POLLING_INTERVAL = 300  # 5 minutes
+
+# Price thresholds for alerts
+PRICE_THRESHOLDS = {
+    'oro': 2000.0,
+    'argento': 25.0,
+    'petrolio': 70.0,
+    'rame': 4.0
+}
+
 # List of available tickers for easy reference
 AVAILABLE_TICKERS = list(SYMBOL_MAPPING.keys())
